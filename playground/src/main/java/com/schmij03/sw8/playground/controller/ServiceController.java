b9ab279f4d174ae9d8af08dc6b3ad5422fcd8b19
schmij03 <96172389+schmij03@users.noreply.github.com>
2023-04-29 18:21:00 +0200

    removed unused import

--- playground/src/main/java/com/schmij03/sw8/playground/controller/ServiceController.java
+++ playground/src/main/java/com/schmij03/sw8/playground/controller/ServiceController.java
@@ -1,7 +1,6 @@
 package com.schmij03.sw8.playground.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestParam;
