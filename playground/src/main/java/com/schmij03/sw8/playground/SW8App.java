dd18c804c10e1e949e9828fce9c267bc9e96b880
schmij03 <96172389+schmij03@users.noreply.github.com>
2023-04-29 17:24:17 +0200

    nötige java files erstellt

---
+++ playground/src/main/java/com/schmij03/sw8/playground/SW8App.java
@@ -0,0 +1,14 @@
+package com.schmij03.sw8.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SW8App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SW8App.class, args);
+	}
+
+	
+}
