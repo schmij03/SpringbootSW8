de631676dbf779a3a8b9f721cb92fde0d3ad8949
schmij03 <96172389+schmij03@users.noreply.github.com>
2023-04-29 17:13:29 +0200

    resetted the project

--- playground/src/main/java/com/schmij03/sw8/playground/PlaygroundApplication.java
+++ playground/src/main/java/com/schmij03/sw8/playground/PlaygroundApplication.java
@@ -2,6 +2,8 @@ package com.schmij03.sw8.playground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PlaygroundApplication {
@@ -10,4 +12,17 @@ public class PlaygroundApplication {
 		SpringApplication.run(PlaygroundApplication.class, args);
 	}
 
+	@RestController
+	public class SentimentController {
+
+		@GetMapping("/ping")
+		public String ping() {
+			return "Sentiment app is up and running!";
+		}
+
+		@GetMapping("/count")
+		public int count() {
+			return 42;
+		}
+	}
 }
