c0495a245728fa662db57c5707f14dd934775feb
schmij03 <96172389+schmij03@users.noreply.github.com>
2023-05-23 14:52:47 +0200

    updated the controller app

--- playground/src/main/java/com/schmij03/sw8/playground/controller/ServiceController.java
+++ playground/src/main/java/com/schmij03/sw8/playground/controller/ServiceController.java
@@ -1,5 +1,4 @@
 package com.schmij03.sw8.playground.controller;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
